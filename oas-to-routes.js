ff22a9a8939ade9056228b65b59d837903879d82
Erik Landvall <erik@landvall.se>
2025-07-22 15:46:12 +0200

    Added support for a bootstrap process that builds routes automatically if an  or  field is defined

---
+++ oas-to-routes.js
@@ -0,0 +1,29 @@
+export function locate(locator)
+{
+  const router = locator.locate('@superhero/http-server-using-oas')
+  return new OasToRoutes(router)
+}
+
+export default class OasToRoutes
+{
+  constructor(router)
+  {
+    this.router = router
+  }
+
+  bootstrap()
+  {
+    for(const [ path, operations ] of Object.entries(this.oas.paths))
+    {
+      for(const [ method, operation ] of Object.entries(operations))
+      {
+        const dispatcher = operation['operationId'] ?? operation['x-dispatcher']
+
+        if('string' === typeof dispatcher)
+        {
+          this.router.setOasRoute(path, method, dispatcher, operation['x-middlewares'] ?? [])
+        }
+      }
+    }
+  }
+}
